245f9d58eef362daf09fb504e6757bd9b49dfe95
Anurag sharma <anuragsharma140897@gmail.com>
2024-03-06 13:27:35 +0530

    chnages to ec2 keys

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   ami                         = "ami-07d9b9ddc6cd8dd30"
   instance_type               = "t2.micro"
-  key_name                    = "ec2-key"
+  key_name                    = "keypair"
   subnet_id                   = aws_subnet.public[count.index].id
   vpc_security_group_ids      = [aws_security_group.allow_tls.id]
   associate_public_ip_address = true
@@ -12,14 +12,14 @@ resource "aws_instance" "web" {
   }
 
   provisioner "file" {
-    source      = "./ec2-key.pem"
-    destination = "/home/ec2-user/ec2-key.pem"
+    source      = "./keypair.pem"
+    destination = "/home/ec2-user/keypair.pem"
 
     connection {
       type        = "ssh"
       host        = self.public_ip
       user        = "ec2-user"
-      private_key = file("../ec2-key.pem")
+      private_key = file("../keypair.pem")
     }
   }
 }
@@ -27,7 +27,7 @@ resource "aws_instance" "web" {
 resource "aws_instance" "db" {
   ami                    = "ami-07d9b9ddc6cd8dd30"
   instance_type          = "t2.micro"
-  key_name               = "ec2-key"
+  key_name               = "keypair.pem"
   subnet_id              = aws_subnet.private.id
   vpc_security_group_ids = [aws_security_group.allow_tls_db.id]
 
